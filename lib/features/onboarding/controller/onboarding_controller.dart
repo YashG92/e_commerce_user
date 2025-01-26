9e780b7101a7a3c18489127714d8f8c57e9fc831
variskadri92 <dzire192@gmail.com>
2025-01-26 13:21:21 +0530

    onboarding controller

--- lib/features/onboarding/controller/onboarding_controller.dart
+++ lib/features/onboarding/controller/onboarding_controller.dart
@@ -1,5 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class OnboardingController extends GetxController{
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
 
+//variable declare
+      final pageController = PageController();
+      Rx<int> currentPageIndex = 0.obs;
+//Update current index when page scroll
+      void updatePageIndex(index){
+        currentPageIndex.value = index;
+      }
+//Update dot navigation
+void dotNavigationClick(int index){
+        currentPageIndex.value = index;
+        pageController.jumpToPage(index);
+
+}
+//jump  to next page
+void nextPage(){
+      if(currentPageIndex.value==2){
+        //get.to.loginscreen
+      }
+      else{
+        int nextPage = currentPageIndex.value+1;
+        pageController.jumpToPage(nextPage);
+      }
+}
+//jump to last page
+  void skipPage(){
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
 }
