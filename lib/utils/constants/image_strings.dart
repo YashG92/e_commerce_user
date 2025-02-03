1a98dc641079434c84b766a84da2056592d26928
variskadri92 <dzire192@gmail.com>
2025-02-03 13:50:24 +0530

    search bar and horizontal list view for categories

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -11,4 +11,27 @@ class ImageStrings {
   static const String googleLogo = "assets/logo/google-icon.png";
   static const String verifyEmailGif = "assets/images/animations/verifyemailgif.json";
   static const String successGif = "assets/images/animations/successgif.json";
+
+  // -- Category Icons
+  static const String sportIcon = "assets/icons/categories/icons8-bowling-64.png";
+  static const String clothIcon = "assets/icons/categories/icons8-tailors-dummy-64.png";
+  static const String shoeIcon = "assets/icons/categories/icons8-shoes-64.png";
+  static const String cosmeticsIcon = "assets/icons/categories/icons8-cosmetics-64.png";
+  static const String animalIcon = "assets/icons/categories/icons8-dog-heart-64.png";
+  static const String toyIcon = "assets/icons/categories/icons8-wooden-toy-car-50.png";
+  static const String furnitureIcon = "assets/icons/categories/icons8-dining-chair-64.png";
+  static const String jeweleryIcon = "assets/icons/categories/icons8-sparkling-diamond-64.png";
+  static const String electronicsIcon = "assets/icons/categories/icons8-smartphone-64.png";
+
+  // -- Brand Icons
+  static const String nikeLogo = "assets/icons/brands/nike.png";
+  static const String adidasLogo = "assets/icons/brands/adidas-logo.png";
+  static const String appleLogo = "assets/icons/brands/apple-logo.png";
+  static const String jordanLogo = "assets/icons/brands/jordan-logo.png";
+  static const String pumaLogo = "assets/icons/brands/puma-logo.png";
+  static const String zaraLogo = "assets/icons/brands/zara-logo.png";
+  static const String kenwoodLogo = "assets/icons/brands/kenwood-logo.png";
+  static const String hermanMillerLogo = "assets/icons/brands/herman-miller-logo.png";
+  static const String ikeaLogo = "assets/icons/brands/ikea_logo.png";
+  static const String acerlogo = "assets/icons/brands/acer_logo.png";
 }
