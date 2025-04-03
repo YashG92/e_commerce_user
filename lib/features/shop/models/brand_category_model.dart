fbf8e35fbd0b423c62482ce3166239ce52cce958
Yash Gotrijiya <95033275+YashG92@users.noreply.github.com>
2025-04-03 20:20:29 +0530

    removal of id parameter for the same

--- lib/features/shop/models/brand_category_model.dart
+++ lib/features/shop/models/brand_category_model.dart
@@ -1,12 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BrandCategoryModel {
-  String? id;
   final String brandId;
   final String categoryId;
 
   BrandCategoryModel({
-    this.id,
     required this.brandId,
     required this.categoryId,
   });
@@ -21,7 +19,6 @@ class BrandCategoryModel {
   factory BrandCategoryModel.fromSnapshot(DocumentSnapshot snapshot) {
     final data = snapshot.data() as Map<String, dynamic>;
     return BrandCategoryModel(
-      id: snapshot.id,
       brandId: data['brandId'] as String,
       categoryId: data['categoryId'] as String,
     );
--- lib/features/shop/models/product_category_model.dart
+++ lib/features/shop/models/product_category_model.dart
@@ -1,12 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProductCategoryModel {
-  final String id;
   final String productId;
   final String categoryId;
 
   ProductCategoryModel({
-    this.id = '',
     required this.productId,
     required this.categoryId,
 });
@@ -23,7 +21,6 @@ class ProductCategoryModel {
 
     final data = doc.data() as Map<String, dynamic>;
     return ProductCategoryModel(
-      id: doc.id,
       productId: data['productId'] as String,
       categoryId: data['categoryId'] as String,
     );
