64d4451243a48dd861813e6c8940bc2119b81d1d
Yash Gotrijiya <95033275+YashG92@users.noreply.github.com>
2025-04-05 12:14:40 +0530

    cart item model

---
+++ lib/features/shop/models/cart_item_model.dart
@@ -0,0 +1,55 @@
+class CartItemModel {
+  String productId;
+  String title;
+  double price;
+  String? image;
+  int quantity;
+  String variationId;
+  String? brandName;
+  Map<String, String>? selectedVariation;
+
+  CartItemModel({
+    required this.productId,
+    required this.quantity,
+    this.variationId = '',
+    this.image,
+    this.price = 0.0,
+    this.title = '',
+    this.brandName,
+    this.selectedVariation,
+});
+
+  ///Empty Cart
+  static CartItemModel empty() => CartItemModel(productId: '',quantity: 0);
+
+  ///Calculate Total Amount
+  String get totalAmount => (price * quantity).toStringAsFixed(1);
+
+  ///Convert a cart item to json
+  Map<String, dynamic> toJson() {
+    return {
+      'productId': productId,
+      'title': title,
+      'price': price,
+      'image': image,
+      'quantity': quantity,
+      'variationId': variationId,
+      'brandName': brandName,
+      'selectedVariation': selectedVariation,
+    };
+  }
+
+  ///Create a cartitem from json
+  factory CartItemModel.fromJson(Map<String, dynamic> json) {
+    return CartItemModel(
+      productId: json['productId'],
+      title: json['title'],
+      price: json['price'],
+      image: json['image'],
+      quantity: json['quantity'],
+      variationId: json['variationId'],
+      brandName: json['brandName'],
+      selectedVariation: json['selectedVariation'] != null ? Map<String, String>.from(json['selectedVariation']) : null,
+    );
+  }
+}
