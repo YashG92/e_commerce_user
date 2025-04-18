1d2caa5400bb353f4d249012836ac554840a99f1
Yash Gotrijiya <95033275+YashG92@users.noreply.github.com>
2025-04-18 16:12:28 +0530

    cart screen empty elevated button issue fixed

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce_user/common/widgets/texts/product_price_text.dart';
 import 'package:e_commerce_user/features/shop/controllers/product/cart_controller.dart';
 import 'package:e_commerce_user/features/shop/screens/cart/widgets/cart_items.dart';
 import 'package:e_commerce_user/features/shop/screens/checkout/checkout_screen.dart';
+import 'package:e_commerce_user/features/shop/screens/global_search_screen.dart';
 
 import 'package:e_commerce_user/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
@@ -37,7 +38,7 @@ class CartScreen extends StatelessWidget {
             animation: ImageStrings.loadingAnimation,
             showAction: true,
             actionText: 'Let\'s fill it',
-            onActionPressed: () => Get.off(() => const NavigationMenu()),
+            onActionPressed: () => Get.to(() => const SearchScreen()),
           );
 
           return controller.cartItems.isEmpty
@@ -50,22 +51,25 @@ class CartScreen extends StatelessWidget {
                 );
         },
       ),
-      bottomNavigationBar: controller.cartItems.isEmpty
-          ? const SizedBox()
-          : Padding(
-              padding: const EdgeInsets.only(
-                left: TSizes.defaultSpace,
-                right: TSizes.defaultSpace,
-                top: TSizes.defaultSpace,
-              ),
-              child: SizedBox(
-                height: 50,
-                child: ElevatedButton(
+      bottomNavigationBar: Obx(() {
+        return controller.cartItems.isEmpty
+            ? const SizedBox()
+            : Padding(
+                padding: const EdgeInsets.only(
+                  left: TSizes.defaultSpace,
+                  right: TSizes.defaultSpace,
+                  top: TSizes.defaultSpace,
+                ),
+                child: SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
                     onPressed: () => Get.to(() => const CheckoutScreen()),
-                    child: Obx(() =>
-                        Text('Checkout ₹${controller.totalCartSalePrice.value}'))),
-              ),
-            ),
+                    child: Text(
+                        'Checkout ₹${controller.totalCartSalePrice.value}'),
+                  ),
+                ),
+              );
+      }),
     );
   }
 }
